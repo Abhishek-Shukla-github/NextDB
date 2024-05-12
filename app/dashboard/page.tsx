41b0332bec1a8359605283f711ea46ed883b97c0
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2024-05-12 13:17:54 -0400

    Add Rightbar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,14 +6,16 @@ import styles from "../ui/dashboard/dashboard.module.css";
 
 const Dashboard = () => {
   return (
-    <div>
-      <div className={styles.cards}>
-        <Card />
-        <Card />
-        <Card />
-      </div>
-      <div className={styles.chart}>
-        <Chart />
+    <div className={styles.wrapper}>
+      <div className={styles.left}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className={styles.chart}>
+          <Chart />
+        </div>
       </div>
       <div className={styles.rightBar}>
         <Rightbar />
