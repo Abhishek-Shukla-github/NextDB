d57da2a8c32094688d7bac15bea33a4d9f8cef1a
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2024-05-18 00:53:00 -0400

    Add Password hashing

--- app/test/actions.ts
+++ app/test/actions.ts
@@ -2,16 +2,20 @@
 import { redirect } from "next/navigation";
 import { Product, User } from "../lib/model";
 import { revalidatePath } from "next/cache";
+import bcrypt from "bcrypt"
 
 export const addUser = async (formData) => {
     console.log("formData",formData)
     const {username, email, password, isAdmin, isActive, address} = Object.fromEntries(formData)
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
     try{
         const newUser = new User({
             username, 
             email,
-            password,
+            password: hashedPassword,
             isAdmin,
             isActive,
             address
